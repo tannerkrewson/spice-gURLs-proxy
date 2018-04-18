c71dc70a9cb3e4b99a9ab54b1f669d050a3bcf21
Brandon Heisserer <brandonheisserer@gmail.com>
2018-04-17 20:27:25 -0500

    Documented CacheItem

--- src/edu/truman/spicegURLs/proxy/CacheItem.java
+++ src/edu/truman/spicegURLs/proxy/CacheItem.java
@@ -5,6 +5,15 @@ import java.awt.event.ActionListener;
 import java.util.Hashtable;
 import javax.swing.Timer;
 
+/**
+ * An object which keeps track of cache information and automatically
+ * self-terminates after a given period of time.
+ * @author Brandon Crane
+ * @author Brandon Heisserer
+ * @author Tanner Krewson
+ * @author Carl Yarwood
+ * @version 17 April 2017
+ */
 public class CacheItem {
 	public static final int MS_PER_MIN = 60000;
 	public static final int DELAY = 1 * MS_PER_MIN; //43200 min in a month
@@ -12,6 +21,12 @@ public class CacheItem {
 	private Hashtable<String, CacheItem> cacheList;
 	private Timer time;
 	
+	/**
+	 * Creates the object from a request and is given access to the
+	 * list in which it is held.
+	 * @param request the page being cached
+	 * @param cacheList the list in which this object is contained
+	 */
 	public CacheItem(String request, Hashtable<String, CacheItem> cacheList) {
 		this.request = request.toLowerCase();
 		this.cacheList = cacheList;
@@ -23,6 +38,12 @@ public class CacheItem {
 		});
 	}
 
+	/**
+	 * Compares the given request with this object's request
+	 * and returns whether or not they are equivalent.
+	 * @param request the request to compare
+	 * @return the comparison
+	 */
 	public boolean isSameRequest(String request) {
 		if (this.request == request.toLowerCase()) {
 			return true;
@@ -30,10 +51,20 @@ public class CacheItem {
 		return false;
 	}
 	
+	/**
+	 * Resets the timer
+	 */
 	public void resetTimer() {
 		time.restart();
 	}
 	
+	//ifModifiedSince
+	
+	//checkForUpdates
+	
+	/**
+	 * Removes this object from the list when it has expired.
+	 */
 	private void terminateCacheItem() {
 		time.stop();
 		cacheList.remove(request);

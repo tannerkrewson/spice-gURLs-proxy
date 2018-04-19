c7b6c6813305c54620e7a610ae781ca3ab0b53ba
tannerkrewson <tannerkrewson@gmail.com>
2018-04-18 21:06:43 -0500

    Added docs for ProxySession, and check the cache with all requests

--- src/edu/truman/spicegURLs/proxy/ProxySession.java
+++ src/edu/truman/spicegURLs/proxy/ProxySession.java
@@ -1,11 +1,6 @@
 package edu.truman.spicegURLs.proxy;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.net.*;
 import java.util.Date;
 import java.util.Scanner;
@@ -23,11 +18,22 @@ public class ProxySession implements Runnable {
 	private Socket client;
 	private Cache cache;
 	
+	/**
+	 * Creates an instance of a ProxySession
+	 * @param client the machine connected to this proxy
+	 * @param cache a reference to the global cache
+	 */
 	public ProxySession(Socket client, Cache cache) {
 		this.client = client;
 		this.cache = cache;
 	}
 	
+	/**
+	 * Looks into the connection and returns the HTTP header
+	 * as a String array, split on spaces.
+	 * @return string array of the parts of the header
+	 * @throws Exception
+	 */
 	private String[] getHeaderOfRequest() throws Exception {
 		BufferedReader inStream
 			= new BufferedReader(new InputStreamReader(client.getInputStream()));
@@ -45,8 +51,16 @@ public class ProxySession implements Runnable {
 		return requestHeader.split(" ");
 	}
 	
-	// TODO: We can also check for 304 in this method
-	private URL getURLFromRequest(String[] req) throws FileNotFoundException, IOException {	
+	/**
+	 * Gets and validates the URL out of an HTTP request
+	 * @param req an HTTP request header, split on spaces
+	 * @return the URL from the request
+	 * @throws FileNotFoundException request doesn't start with /proxy/
+	 * @throws IOException URL from request is malformed
+	 */
+	private URL getURLFromRequest(String[] req) throws FileNotFoundException, IOException {
+		// TODO: We can also check for 304 in this method
+		
 		if (!req[1].startsWith("/proxy/")) {
 			throw new FileNotFoundException("Ignored: " + req[1]);
 		}
@@ -66,6 +80,15 @@ public class ProxySession implements Runnable {
 		return new URL(url);
 	}
 	
+	/**
+	 * Makes a request to the given URL with the given if-modified-since
+	 * time, and returns the contents of the result as a string, usually 
+	 * raw HTML.
+	 * @param url the URL to send a GET request to
+	 * @param ims the if-modified-since time
+	 * @return the result of the request
+	 * @throws Exception
+	 */
 	private String getResponseFromURL (URL url, Date ims) throws Exception {
 		HttpURLConnection con = (HttpURLConnection)url.openConnection();
 		con.setRequestMethod("GET");
@@ -79,16 +102,34 @@ public class ProxySession implements Runnable {
 	    return responseBody;
 	}
 	
+	/**
+	 * Send a response to our client with the given code and no further
+	 * content.
+	 * @param code HTTP response code
+	 * @throws Exception
+	 */
 	private void sendResponse(String code) throws Exception {
 		sendResponse(code, "");
 	}
 	
+	/**
+	 * Send a response to our client with the given code and given response 
+	 * attached.
+	 * @param code HTTP response code
+	 * @param response HTML or things to send back
+	 * @throws Exception
+	 */
 	private void sendResponse(String code, String response) throws Exception {
         String httpResponse = "HTTP/1.1 " + code + "\r\n\r\n" + response;
     	client.getOutputStream().write(httpResponse.getBytes("UTF-8"));
         client.close();
 	}
 	
+	/**
+	 * Parses the client's request, requests it, and sends the response 
+	 * back to them.
+	 * @see java.lang.Runnable#run()
+	 */
 	@Override
 	public void run() {
 		try {
@@ -99,14 +140,14 @@ public class ProxySession implements Runnable {
 			// This is where we'll check for 304
 			String response;
 			
-			/*CacheItem ci = cache.get(urlToGet);
+			CacheItem ci = cache.getItem(urlToGet.toString());
 			if (ci != null) {
 				response = getResponseFromURL(urlToGet, ci.getLastModified());
 				ci.setPage(response);
-			} else {*/
+			} else {
 				response = getResponseFromURL(urlToGet, new Date());
 				cache.addItem(new CacheItem(urlToGet, response));
-			//}
+			}
 			
 			sendResponse("200 OK", response);
 	     

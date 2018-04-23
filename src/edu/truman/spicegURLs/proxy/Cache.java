df545d4d0c4788d352052a46e555e26a385c19c4
Brandon Heisserer <brandonheisserer@gmail.com>
2018-04-22 19:40:48 -0500

    Documented and formatted Cache

--- src/edu/truman/spicegURLs/proxy/Cache.java
+++ src/edu/truman/spicegURLs/proxy/Cache.java
@@ -5,11 +5,25 @@ import java.util.Map;
 import java.util.Iterator;
 import java.util.Set;
 import java.lang.Runtime;
+
+/**
+ * An object which stores all of the CacheItems for the Proxy 
+ * @author Brandon Crane
+ * @author Brandon Heisserer
+ * @author Tanner Krewson
+ * @author Carl Yarwood
+ * @version 22 April 2017
+ */
 public class Cache {
 
 	HashMap<String, CacheItem> CacheStore = null;
-	public Cache(){
-		try{
+	
+	/**
+	 * Creates a Cache object which checks for a stored file
+	 * and loads it if there is one.
+	 */
+	public Cache () {
+		try {
 			FileInputStream fis = new FileInputStream("hashcache.ser");
 			ObjectInputStream ois = new ObjectInputStream(fis);
 			CacheStore = (HashMap<String,CacheItem>) ois.readObject();
@@ -17,46 +31,65 @@ public class Cache {
 			fis.close();
 			Set<String> startUpList = CacheStore.keySet();
 			Iterator<String> itrSet = startUpList.iterator();
-			while(itrSet.hasNext()){
+			while (itrSet.hasNext()) {
 				String key = itrSet.next();
 				CacheStore.get(key).initTimer();
 				CacheStore.get(key).requestUpdatedPage();
 			}
-		}catch(Exception e){
+		} catch (Exception e) {
 			System.err.println("Cache either does not exist or could not be processed\n creating a new Cache");
 			CacheStore = new HashMap<String,CacheItem>();
 		}
-		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable(){
-
+		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
 			@Override
-			public void run() {
+			public void run () {
 				shutDown();
-			}
-			
+			}	
 		}));
 	}
-	public void addItem(CacheItem in){
-		if(!this.CacheStore.containsKey(in.getRequestURL())){
+	
+	/**
+	 * Adds a CacheItem to the CacheStore
+	 * @param in the CacheItem to store
+	 */
+	public void addItem (CacheItem in) {
+		if (!this.CacheStore.containsKey(in.getRequestURL())) {
 			this.CacheStore.put(in.getRequestURL(),in);
 		}
-		else{
+		else {
 			this.CacheStore.replace(in.getRequestURL(),in);
 		}
 	}
-	public void removeItem(String key){
+	
+	/**
+	 * Removes a CacheItem from the CacheStore.
+	 * @param key the key of the CacheItem to remove
+	 */
+	public void removeItem (String key) {
 		CacheStore.remove(key);
 	}
-	public CacheItem getItem(String key){
+	
+	/**
+	 * Returns the CacheItem corresponding to the given key.
+	 * Returns NULL if the item is not in the Cache.
+	 * @param key the key of the CacheItem to return
+	 * @return
+	 */
+	public CacheItem getItem (String key) {
 		return CacheStore.get(key);
 	}
-	public void shutDown(){
-		try{
+	
+	/**
+	 * Saves the Cache upon termination.
+	 */
+	public void shutDown () {
+		try {
 			FileOutputStream foe = new FileOutputStream("hashcache.ser", false);
 			ObjectOutputStream oos = new ObjectOutputStream(foe);
 			oos.writeObject(CacheStore);
 			oos.close();
 			foe.close();
-		}catch(Exception e){
+		} catch (Exception e) {
 			System.err.println("Could not write Cache");
 			System.err.println(e);
 		}
